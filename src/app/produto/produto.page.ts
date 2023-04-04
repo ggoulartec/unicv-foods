3b756f9bbacc8f76ec4bba193e280f81654e7b7e
ggoulartec <gustavo.49472-2021@aluno.unicv.edu.br>
2023-04-04 17:22:12 -0300

    Update produto.page.ts

--- src/app/produto/produto.page.ts
+++ src/app/produto/produto.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-produto',
   templateUrl: './produto.page.html',
   styleUrls: ['./produto.page.scss'],
 })
-export class ProdutoPage implements OnInit {
+export class ProdutoPage {
 
-  constructor() { }
+  public pathImg = '../../assets/img';
+  public quantidade = 0;
 
-  ngOnInit() {
+  public produto = {
+    codigo: 0,
+    nome: '',
+    categoria: 0,
+    descricao: '',
+    valor: 0.00,
+    imagens: []
   }
 
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.produto = params['produto'];
+    });
+  }
+
+  public increment(){
+    this.quantidade += 1;
+  }
+
+  public decrement(){
+    if(this.quantidade > 0){
+      this.quantidade -= 1;
+    }
+  }
 }
